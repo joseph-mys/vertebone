d624e64e80dba6d9505ca7cf347977ffc338ee50
zymhmss <137022751+zymhmss@users.noreply.github.com>
2024-09-01 17:27:57 +0630

    Initialized the project with a canvas preparation

---
+++ script.js
@@ -0,0 +1,22 @@
+//let's start with a canvas
+let canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+// now set canvas height and width same as window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//need to remove unwatned default margin of body and hide overflow for full canvas
+document.body.style.margin = "0px";
+document.body.style.overflow = "hidden";
+document.body.style.backgroundColor = "black";
+
+//segment
+
+//leg
+
+//body
+
+//head
+
+//monster
